049a3e2815562d6131f015080c16181e563031f8
Cameron Grams <cameron.grams.2@gmail.com>
2018-11-19 22:47:53 -0500

    added CORS permissions

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    next();
+});
+
 app.use(express.static( path.join( __dirname,  '/build') ) );
 
 app.get('/*', function (req, res) {
-    console.log( 'in server with req: ', req );
   return res.sendFile( path.join( __dirname, 'build', 'index.html') );
 });
 
